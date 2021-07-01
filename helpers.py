6d551403e83e047585e060d5530834c1dd4de60d
Takfes <Takis_a29@hotmail.com>
2021-07-01 03:07:40 +0300

    changes in summary and details scripts

--- helpers.py
+++ helpers.py
@@ -1,43 +1,97 @@
-
+import re
 import time
 import requests
+import numpy as np
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+def yield_url(months,adults):
+    # base url
+    base = f'https://www.airbnb.com/s/Rethimnon--Greece/homes?adults={adults}'
+    # filter for certain room types
+    room_types = '''
+        &room_types%5B%5D=Entire%20home%2Fapt
+        &room_types%5B%5D=Private%20room
+        &room_types%5B%5D=Hotel%20room
+        &search_type=filter_change
+        '''.replace('\n','').replace(" ", "").strip()
+    # add flexible dates to get cost estimates
+    if isinstance(months,list):
+        flexible_dates = ''
+        for m in months: 
+            flexible_dates = flexible_dates + f'&flexible_trip_dates%5B%5D={m}'
+        flexible_dates = flexible_dates + '&date_picker_type=flexible_dates'
+    return base+room_types+flexible_dates
+
 
 def parse_page(soup):
-    
-    listing_items = soup.find_all('div', class_='_8ssblpx')
+    # regex float numbers
+    regex_float = r"[-+]?\d*\.\d+|\d+"
+    # instantiate a dict object to hold room specs
     listing_dict = {}
-
-    for i,r in enumerate(listing_items,start=1):
-        
-        # print(f'{25*"-"}{str(i)}{25*"-"}')        
-        # instantiate a temp dict object to hold room specs
-        temp_room = {}
-        
-        # get combinded text
-        temp_room['combined_text'] = r.text    
-
-        # get text list
-        temp_room['list_text'] = [x.text for x in r.find('div', class_='_12oal24')]
-
-        # get urls
-        temp_room_key = r.find('a', href=True).get('href')
-        temp_room['parsed_url'] = temp_room_key
-        temp_room['functional_url'] = f'airbnb.com{temp_room_key}'
-        
-        # insert temp dict into the general dict
-        listing_dict['room'+str(i)] = temp_room
+    # grab listing containers
+    containers = soup.find_all('div', class_='_8ssblpx')
+    # if there are containers
+    if containers:
+        # loop through containers to extract info
+        for i,r in enumerate(containers,start=1):
+            # instantiate a temp dict object to hold individual room specs
+            temp_room = {}
+            # get combinded text
+            temp_room['combined_text'] = r.text    
+            # get text list
+            temp_room['list_text'] = [x.text for x in r.find('div', class_='_12oal24')]
+            # get urls
+            temp_room_key = r.find('a', href=True).get('href')
+            temp_room['parsed_url'] = temp_room_key
+            temp_room['functional_url'] = f'airbnb.com{temp_room_key}'
+            # header
+            temp_room['header'] = r.find('div',class_="_1olmjjs6").text
+            # title
+            temp_room['title'] = r.find('div',class_="_5kaapu").text
+            # subtitle specs
+            specs_list = [x.text for x in r.find_all('span',class_="_3hmsj")]
+            for spec in specs_list :
+                if 'guest' in spec:
+                    temp_room['no_guests'] = float(re.findall(regex_float,spec)[0])
+                if 'bedroom' in spec:
+                    temp_room['no_bedrooms'] = float(re.findall(regex_float,spec)[0])
+                if ('bed' in spec) & ('room' not in spec):
+                    temp_room['no_beds'] = float(re.findall(regex_float,spec)[0])
+                if 'bath' in spec:
+                    temp_room['no_baths'] = float(re.findall(regex_float,spec)[0])
+            # rating
+            rating_raw = r.find('span',class_='_10fy1f8')
+            if rating_raw:
+                temp_room['rating'] = float(r.find('span',class_='_10fy1f8').text)
+            else :
+                temp_room['rating'] = 0.0
+            # reviews
+            reviews_string = r.find('span',class_='_a7a5sx')
+            if reviews_string:
+                reviews_string = reviews_string.text.replace('(','').replace(')','')
+                temp_room['no_reviews'] = [int(word) for word in reviews_string.split() if word.isdigit()][0]
+            else : 
+                temp_room['no_reviews'] = 0.0
+            # price per night
+            price_string = r.find("span", class_="_155sga30")
+            if price_string:
+                price_string = price_string.text
+                temp_room['price'] = int(re.findall(regex_float,price_string)[0])
+            else : 
+                temp_room['price'] = 0.0
+            # insert temp dict into the general dict
+            listing_dict['room'+str(i)] = temp_room
 
     # dataframe manipulation
     # turn dict into dataframe
-    yoki = pd.DataFrame().from_dict(listing_dict,orient='index')
-    # split column into multiple columns
-    list_columns = ['title','badge','capacity','amenities','reviews']
-    soki = yoki.list_text.apply(pd.Series)
-    soki.columns = list_columns
-    # merge initial with derived above df
-    moki = yoki.merge(soki,left_index=True, right_index=True)
-    return moki
+    if listing_dict:
+        yoki = pd.DataFrame().from_dict(listing_dict,orient='index')
+        # split column into multiple columns
+        list_columns = ['headline','badge','capacity','amenities','dates','reviews_price']
+        soki = yoki.list_text.apply(pd.Series)
+        soki.columns = list_columns
+        # merge initial with derived above df
+        moki = yoki.merge(soki,left_index=True, right_index=True)
+        return moki
--- main.py
+++ main.py
@@ -4,34 +4,35 @@ import requests
 import pandas as pd
 from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-from helpers import parse_page
+from helpers import parse_page, yield_url
 
 # beautiful soup used with pregenerated urls
 # https://www.airbnb.com/s/Rethimnon--Greece/homes?pagination_search=true&items_offset=20&room_types%5B%5D=Entire%20home%2Fapt&room_types%5B%5D=Private%20room&room_types%5B%5D=Hotel%20room&search_type=filter_change
-    
+
+pages = []
+months = ['july','august']
+adults = 6
+counter = 0    
 max_offset = 300
 offset_step = 20 # dont change
-pages = []
-counter = 0
 
 if __name__ == '__main__':
 
-    for i in range(0,max_offset+offset_step,offset_step):
+    for i in range(0,max_offset,offset_step):        
         
-        url = f'https://www.airbnb.com/s/Rethimnon--Greece/homes?pagination_search=true&items_offset={str(i)}&room_types%5B%5D=Entire%20home%2Fapt&room_types%5B%5D=Private%20room&room_types%5B%5D=Hotel%20room&search_type=filter_change'
-        
+        # paginated urls
+        base = yield_url(months,adults)
+        url = base + f'&pagination_search=true&items_offset={str(i)}'
         try :
             page = requests.get(url)
             soup = BeautifulSoup(page.content, 'html.parser')
             pages.append(soup)
             counter += 1
-            print(f'> parsed page {str(i)}')
+            print(f'> parsed page {str(counter)}({round(((i+offset_step)/max_offset)*100,2)}%)')
         except Exception as e:
             print(f'error during : {url}' )
-        
-    print(f'parsed pages counter : {str(counter)}')
-    print(f'parsed pages list length : {len(pages)}')
 
     # given a page (soup), parse all of its listings
     df_list = [parse_page(page) for page in pages]
@@ -39,16 +40,23 @@ if __name__ == '__main__':
     df = pd.concat(df_list).reset_index(drop=True)
     print(f'parsed df shape : {df.shape}')
     # add custom column to determine uniqueness of a listing
-    df['unique_string'] = (df.title + df.reviews)
+    df['unique_string'] = (df.title + df.reviews_price)
     print(f'"unique_string" distinct items : {df.unique_string.nunique()}')
     # drop columns based on custom column from above
     df.drop_duplicates(subset=['unique_string'],inplace=True)
     print(f'final df shape : {df.shape}')
     
-    
-# code to parse individual page
 
+df.to_clipboard()
+df.shape
+
+# ---------------------------------------------------------------------------------    
+# ---------------------------------------------------------------------------------
+# code to parse individual page
 # selenium for traversing the site
+# ---------------------------------------------------------------------------------    
+# ---------------------------------------------------------------------------------
+
 CHROMEDRIVER = '/home/takis/Desktop/sckool/chromedriver_linux64/chromedriver'
 URL = 'https://www.airbnb.com/s/Rethimnon--Greece/homes'
 
@@ -63,64 +71,107 @@ home_url_list = df.functional_url.tolist()
 
 parsing_dict = {'title':'_xcsyj0',
                 'subtitle': '_tqmy57',
-                'about_host':'//*[@id="site-content"]/div/div[1]/div[3]/div[1]/div/div[4]/div/div[2]/div/div[3]',
-                'open_about_host':'//*[@id="site-content"]/div/div[1]/div[3]/div[1]/div/div[4]/div/div[2]/div[3]/div/button',
-                'close_about_host':'//*[@id="decs_root"]/div[10]/section/div/div/div/div/div/div/div/div/div[1]/div/button',
-                'open_amenities':'//*[@id="site-content"]/div/div[1]/div[3]/div[1]/div/div[6]/div/div[2]/div[4]/a',
-                'close_amenities':'/html/body/div[11]/section/div/div/div[1]/button',
-                'about_location':'//*[@id="site-content"]/div/div[1]/div[5]/div/div/div/div[2]/div[4]/a/span/span[1]',
-                'house_rules':'//*[@id="site-content"]/div/div[1]/div[7]/div/div/div/div[2]/div/div/div[2]/div[1]/div/div',
-                'calendar':'_cvkwaj'}
+                'about_place_open':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[3]/div[1]/div/div[3]/div/div[2]/div[2]/div/button',
+                'about_place_parse':'/html/body/div[10]/section/div/div/div/div/div/div/div/div/div[2]/div/div/section',
+                'about_place_close':'/html/body/div[10]/section/div/div/div/div/div/div/div/div/div[1]/div/button',
+                'amenities_open':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[3]/div[1]/div/div[5]/div/div[2]/div[4]/a',
+                'amenities_close':'/html/body/div[10]/section/div/div/div[1]/button',
+                'about_location_open':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[5]/div/div/div/div[2]/div[4]/a',
+                'about_location_parse':'/html/body/div[10]/section/div/div/div[3]/div/div[1]/div[2]/div/span/div/span',
+                'about_location_close':'/html/body/div[10]/section/div/div/div[1]/button',
+                'house_rules_open':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[7]/div/div/div/div[2]/div/div[2]/div[1]/div/div/div[7]/a',
+                'house_rules_parse':'/html/body/div[10]/section/div/div/div[3]/div/div/section',
+                'house_rules_close':'/html/body/div[10]/section/div/div/div[1]/button',
+                'calendar':'_cvkwaj',
+                'price_xpath':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[3]/div[2]/div/div/div[1]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/span[1]',
+                'rating_xpath':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[3]/div[2]/div/div/div[1]/div/div/div/div/div/div[1]/div[1]/div[2]/span/span[2]',
+                'review_xpath':'/html/body/div[4]/div/div[1]/div/div/div/div/div[1]/main/div/div[1]/div[3]/div[2]/div/div/div[1]/div/div/div/div/div/div[1]/div[1]/div[2]/span/a/span'}
 
 listing_details = {}
 
-for hurl in home_url_list[:2]:
+for hurl in home_url_list:
 
     print(hurl)
     
     url = 'http://'+hurl
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    # page = requests.get(url)
+    # soup = BeautifulSoup(page.content, 'html.parser')
 
+    current_listing = {}
     driver.get(url)
     time.sleep(2) # Let the user actually see something!
-
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'lxml')
-    current_listing = {}
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2) # Let the user actually see something!
+    
+    # price
+    try:
+        current_listing['price'] = driver.find_element_by_xpath(parsing_dict['price_xpath']).text
+    except:
+        current_listing['price'] = 'NA'
+    
+    # rating
+    try:
+        current_listing['rating'] = driver.find_element_by_xpath(parsing_dict['rating_xpath']).text
+    except:
+        current_listing['rating'] = 'NA'
+    
+    # reviews
+    try:
+        current_listing['reviews'] = driver.find_element_by_xpath(parsing_dict['review_xpath']).text
+    except:
+        current_listing['reviews'] = 'NA'
     
     # title
-    current_listing['title'] = soup.find('div',class_=parsing_dict['title']).text
+    try:
+        current_listing['title'] = soup.find('div',class_=parsing_dict['title']).text
+    except:
+        current_listing['title'] = 'NA'
 
     # subtitle
     boo = [x.text for x in soup.find('div',class_=parsing_dict['subtitle']).find_all('span') if x.text != ' · ']
     current_listing['subtitle'] = "-".join(boo)
 
-    # # about_host
-    # bar = driver.find_element_by_xpath(parsing_dict['about_host'])
-    # bar.text
+    # about place - OR in the url &modal=DESCRIPTION
+    try:
+        # driver.find_element_by_xpath(parsing_dict['about_place_open']).click()
+        driver.find_elements_by_xpath("//*[contains(text(), 'Show more')]")[0].click()
+        # current_listing['about_place'] = driver.find_element_by_xpath(parsing_dict['about_place_parse']).text.replace('\n',' ')
+        current_listing['about_place'] = BeautifulSoup(driver.page_source,'lxml').text.replace('\n',' ')
+        # driver.find_element_by_xpath(parsing_dict['about_place_close']).click()
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    except:
+        current_listing['about_place'] = 'NA'
 
-    # # about_host_clickable
-    # driver.find_element_by_xpath(parsing_dict['open_about_host']).click()
-    # zoo = [x.text.replace('\n',' ') for x in driver.find_elements_by_xpath('/html/body/div[10]/section/div/div/div/div/div/div/div/div/div[2]/div/div')]
-    # current_listing['about_host'] = " ".join(zoo)
-    # driver.find_element_by_xpath(parsing_dict['close_about_host']).click()
+    # amenities /room/1928432/amenities
+    try:
+        # driver.find_element_by_xpath(parsing_dict['amenities_open']).click()
+        driver.find_elements_by_xpath("//*[contains(text(), 'Show all')]")[1].click()
+        soo = [x.text.replace('\n',' ') for x in driver.find_elements_by_class_name('_aujnou')]
+        current_listing['amenities'] = " ".join(soo)
+        # driver.find_element_by_xpath(parsing_dict['amenities_close']).click()
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    except:
+        current_listing['amenities'] = 'NA'
+        
+    # # about_location
+    # driver.find_element_by_xpath(parsing_dict['about_location_open']).click()
+    # # current_listing['about_location'] = driver.find_element_by_xpath(parsing_dict['about_location_parse']).text
+    # current_listing['about_location'] = BeautifulSoup(driver.page_source,'lxml').text.replace('\n',' ')
+    # # driver.find_element_by_xpath(parsing_dict['about_location_close']).click()
+    # webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
-    # amenities
-    driver.find_element_by_xpath(parsing_dict['open_amenities']).click()
-    time.sleep(2)
-    soo = [x.text.replace('\n',' ') for x in driver.find_elements_by_class_name('_aujnou')]
-    current_listing['amenities'] = " ".join(soo)
-    driver.find_element_by_xpath(parsing_dict['close_amenities']).click()
-
-    # about_location
-    current_listing['about_location'] = driver.find_element_by_xpath(parsing_dict['about_location']).text
-
-    # house_rules
-    current_listing['house_rules'] = driver.find_element_by_xpath(parsing_dict['house_rules']).text.replace('\n',' ')
+    # # house_rules
+    # driver.find_element_by_xpath(parsing_dict['house_rules_open']).click()
+    # current_listing['house_rules'] = driver.find_element_by_xpath(parsing_dict['house_rules_parse']).text.replace('\n',' ')
+    # current_listing['house_rules'] = BeautifulSoup(driver.page_source,'lxml').text.replace('\n',' ')
+    # driver.find_element_by_xpath(parsing_dict['house_rules_close']).click()
+    # webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()    
+    
+    # cancellation policy
+    # driver.find_elements_by_xpath("//*[contains(text(), 'Show more')]")[8].click()
 
     # map coordinates
-    url_string = str(page_source)
+    url_string = str(driver.page_source)
     p_lat = re.compile(r'"lat":([-0-9.]+),')
     p_lng = re.compile(r'"lng":([-0-9.]+),')
     try:
@@ -131,6 +182,8 @@ for hurl in home_url_list[:2]:
     current_listing['coordindates'] = (lat,lng)
 
     # calendar
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
     oto = soup.find_all('table', parsing_dict['calendar'])
     current_month = datetime.now().month
 
@@ -144,7 +197,7 @@ for hurl in home_url_list[:2]:
                     availability = dd.find('div')['data-is-day-blocked']
                 except :
                     availability = 'Na'
-                print(f'{date}-{availability}')
+                # print(f'{date}-{availability}')
                 calendar[f'month_{str(current_month)}_day_{date}'] = availability
 
         # next_month
@@ -155,7 +208,7 @@ for hurl in home_url_list[:2]:
                     availability = dd.find('div')['data-is-day-blocked']
                 except :
                     availability = 'Na'
-                print(f'{date}-{availability}')
+                # print(f'{date}-{availability}')
                 calendar[f'month_{str(current_month+1)}_day_{date}'] = availability
 
         bookings = pd.Series(calendar)
@@ -164,6 +217,12 @@ for hurl in home_url_list[:2]:
     else:
         current_listing['bookings'] = None
         current_listing['availability_rate'] = None
-        
-driver.quit()
+
+    listing_details[hurl] = current_listing
+    
+    
+dt = pd.DataFrame().from_dict(listing_details,orient='index').sort_values(by=['availability_rate'])
+print(f'dt.shape:{dt.shape}')
+
+# driver.quit()
 
